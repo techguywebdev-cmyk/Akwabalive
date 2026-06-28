663c5b96bb0ed95d57e79eef31ef73075a99a587
techguywebdev-cmyk <techguywebdev@gmail.com>
2026-06-28 22:47:08 +0000

    Add GhanaEvent type import to EventCard component

--- components/ui/EventCard.tsx
+++ components/ui/EventCard.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { MapPin, Calendar, Clock } from 'lucide-react';
 import clsx from 'clsx';
+import type { GhanaEvent } from '@/lib/types';
 import { formatDistance, cityLabel } from '@/lib/utils';
 
 type Size = 'feature' | 'standard' | 'mini';
